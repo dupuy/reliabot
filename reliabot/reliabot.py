7c0c078c101407dd613f60c2dab7eb0c2e7d65fd
appills <adpilipauskas@gmail.com>
2024-01-11 09:56:44 -0600

    Add pylint hint to line ci is complaining about

--- reliabot/reliabot.py
+++ reliabot/reliabot.py
@@ -144,10 +144,10 @@ RUAMEL_YAML_NOT_FOUND_ERROR_MESSAGE = """
     See https://github.com/dupuy/reliabot/#installation for installation instructions."""
 try:
     # ruamel.yaml preserves comments, PyYAML doesn't.
+    # pylint: disable=ungrouped-imports
     from ruamel.yaml import YAML
 except ModuleNotFoundError as module_not_found:
     error(f"{module_not_found} {RUAMEL_YAML_NOT_FOUND_ERROR_MESSAGE}")
-# pylint: disable=ungrouped-imports
 
 
 DOT_YAML = r"[.]ya?ml"

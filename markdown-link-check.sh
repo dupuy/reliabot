c3d9687040f6ab648a054f6f7dbdcbb7529a9634
Alexander Dupuy <alex@dupuy.us>
2025-07-21 00:13:41 -0400

    refactor: incorporate markdown-link-check as local
    Unmodified version from https://github.com/mineiros-io/pre-commit-hooks/

---
+++ markdown-link-check.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -e
+
+if command -v npm >/dev/null 2>&1; then
+  prefix=$(npm config get prefix)
+  export PATH=${PATH}:${prefix}/bin
+fi
+
+if ! command -v markdown-link-check >/dev/null 2>&1; then
+  echo >&2 "markdown-link-check is not available on this system."
+  echo >&2 "Please install it by running 'npm install -g markdown-link-check'"
+  exit 1
+fi
+
+while getopts ':p' 'OPTKEY'; do
+  case ${OPTKEY} in
+    'p') set +e ;;
+    *)
+        echo "unimplemented option -- ${OPTKEY}" >&2
+        exit 1
+        ;;
+  esac
+done
+
+shift $((OPTIND-1))
+
+TMP_CONFIG="$(mktemp)"
+trap 'rm -f $TMP_CONFIG;' EXIT
+
+cat >"$TMP_CONFIG" <<EOF
+{
+  "replacementPatterns": [
+    {
+      "pattern": "^/",
+      "replacement": "file://$(pwd)/"
+    }
+  ]
+}
+EOF
+
+for file in "$@"; do
+  markdown-link-check -c "$TMP_CONFIG" "$file"
+done
+
+exit 0

c13d58b4bb222fbcea262573886427d380407288
Alexander Dupuy <alex.dupuy@rearc.io>
2026-09-17 17:58:56 -0400

    feat: cancel jobs pending approval on PR close
    Include some adjacent GHA, pre-commit and scripts changes

--- scripts/stalled-actions.sh
+++ scripts/stalled-actions.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+#
+# This information is also available at:
+#
+# https://github.com/dupuy/reliabot/actions?query=is%3Aaction_required%2Cpending%2Cwaiting
+#
+
 status() {
   gh run list --status "$1" --json url -q '.[].url' | sed 's/$/ '"$1"'/'
 }
